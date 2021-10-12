53a1146c7e8cabcc64f3c9b0ed33ce7ac077db33
Krokotsch, Tilman (TP-T) <tilman.krokotsch@iav.de>
2021-10-12 13:46:55 +0200

    Move to version 0.3.0

--- rul_datasets/__init__.py
+++ rul_datasets/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.2"
+__version__ = "0.3.0"
 
 from .adaption import DomainAdaptionDataModule, PretrainingAdaptionDataModule
 from .baseline import BaselineDataModule, PretrainingBaselineDataModule
