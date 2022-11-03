a21fe65b025b5d17d806be8073b7656c21dd42fd
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-11-03 20:33:03 +0100

    Extend features of ssl data module.

--- rul_datasets/ssl.py
+++ rul_datasets/ssl.py
@@ -1,17 +1,19 @@
 """A module with higher-order data modules for semi-supervised learning."""
 import warnings
+from typing import Any, Optional
 
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+from rul_datasets.adaption import AdaptionDataset
 from rul_datasets.core import RulDataModule
-from rul_datasets.adaption import DomainAdaptionDataModule
 
 
-class SemiSupervisedDataModule(DomainAdaptionDataModule):
+class SemiSupervisedDataModule(pl.LightningDataModule):
     """
     A higher-order [data module][pytorch_lightning.core.LightningDataModule] used for
     semi-supervised learning with a labeled data module and an unlabeled one. It
-    behaves exactly the same as [DomainAdaptionDataModule]
-    [rul_datasets.adaption.DomainAdaptionDataModule] but makes sure that both data
-    modules come from the same sub-dataset.
+    makes sure that both data modules come from the same sub-dataset.
 
     Examples:
         >>> import rul_datasets
@@ -20,9 +22,9 @@ class SemiSupervisedDataModule(DomainAdaptionDataModule):
         >>> labeled = rul_datasets.RulDataModule(fd1, 32)
         >>> unlabeled = rul_datasets.RulDataModule(fd1_complement, 32)
         >>> dm = rul_datasets.SemiSupervisedDataModule(labeled, unlabeled)
-        >>> train_1_2 = dm.train_dataloader()
-        >>> val_1, val_2, paired_val_1_2 = dm.val_dataloader()
-        >>> test_1, test_2, paired_test_1_2 = dm.test_dataloader()
+        >>> train_ssl = dm.train_dataloader()
+        >>> val = dm.val_dataloader()
+        >>> test = dm.test_dataloader()
     """
 
     def __init__(self, labeled: RulDataModule, unlabeled: RulDataModule) -> None:
@@ -38,25 +40,41 @@ class SemiSupervisedDataModule(DomainAdaptionDataModule):
             labeled: The data module of the labeled dataset.
             unlabeled: The data module of the unlabeled dataset.
         """
-        super().__init__(labeled, unlabeled)
+        super().__init__()
+
+        self.labeled = labeled
+        self.unlabeled = unlabeled
+        self.batch_size = labeled.batch_size
+
+        self._check_compatibility()
+
+        self.save_hyperparameters(
+            {
+                "fd": self.labeled.reader.fd,
+                "batch_size": self.batch_size,
+                "window_size": self.labeled.reader.window_size,
+                "max_rul": self.labeled.reader.max_rul,
+                "percent_broken_unlabeled": self.unlabeled.reader.percent_broken,
+                "percent_fail_runs_labeled": self.labeled.reader.percent_fail_runs,
+            }
+        )
 
     def _check_compatibility(self) -> None:
-        self.source.check_compatibility(self.target)
-        self.target.reader.check_compatibility(self.target_truncated)
-        if not self.source.reader.fd == self.target.reader.fd:
+        self.labeled.check_compatibility(self.unlabeled)
+        if not self.labeled.reader.fd == self.unlabeled.reader.fd:
             raise ValueError(
                 "FD of source and target has to be the same for "
                 "semi-supervised learning, but they are "
-                f"{self.source.reader.fd} and {self.target.reader.fd}."
+                f"{self.labeled.reader.fd} and {self.unlabeled.reader.fd}."
             )
-        if self.target.reader.percent_broken is None:
+        if self.unlabeled.reader.percent_broken is None:
             warnings.warn(
                 "The unlabeled data is not truncated by 'percent_broken'."
                 "This may lead to unrealistically good results."
                 "If this was intentional, please set `percent_broken` "
                 "to 1.0 to silence this warning."
             )
-        if not self.source.is_mutually_exclusive(self.target):
+        if not self.labeled.is_mutually_exclusive(self.unlabeled):
             warnings.warn(
                 "The data modules are not mutually exclusive. "
                 "This means there is an overlap between labeled and "
@@ -64,3 +82,81 @@ class SemiSupervisedDataModule(DomainAdaptionDataModule):
                 "semi-supervised learning. You can check this by calling "
                 "'is_mutually_exclusive' on a reader or RulDataModule."
             )
+
+    def prepare_data(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Download and pre-process the underlying data.
+
+        This calls the `prepare_data` function for source and target domain. All
+        previously completed preparation steps are skipped. It is called
+        automatically by `pytorch_lightning` and executed on the first GPU in
+        distributed mode.
+
+        Args:
+            *args: Passed down to each data module's `prepare_data` function.
+            **kwargs: Passed down to each data module's `prepare_data` function..
+        """
+        self.labeled.prepare_data(*args, **kwargs)
+        self.unlabeled.prepare_data(*args, **kwargs)
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """
+        Load labeled and unlabeled data into memory.
+
+        Args:
+            stage: Passed down to each data module's `setup` function.
+        """
+        self.labeled.setup(stage)
+        self.unlabeled.setup(stage)
+
+    def train_dataloader(self, *args: Any, **kwargs: Any) -> DataLoader:
+        """
+        Create a data loader of an [AdaptionDataset]
+        [rul_datasets.adaption.AdaptionDataset] using labeled and unlabeled.
+
+        The data loader is configured to shuffle the data. The `pin_memory` option is
+        activated to achieve maximum transfer speed to the GPU.
+
+        Args:
+            *args: Ignored. Only for adhering to parent class interface.
+            **kwargs: Ignored. Only for adhering to parent class interface.
+        Returns:
+            The training data loader
+        """
+        return DataLoader(
+            self._to_dataset("dev"),
+            batch_size=self.batch_size,
+            shuffle=True,
+            pin_memory=True,
+        )
+
+    def val_dataloader(self, *args: Any, **kwargs: Any) -> DataLoader:
+        """
+        Create a data loader of the labeled validation data.
+
+        Args:
+            *args: Ignored. Only for adhering to parent class interface.
+            **kwargs: Ignored. Only for adhering to parent class interface.
+        Returns:
+            The labeled validation data loader.
+        """
+        return self.labeled.val_dataloader()
+
+    def test_dataloader(self, *args: Any, **kwargs: Any) -> DataLoader:
+        """
+        Create a data loader of the labeled test data.
+
+        Args:
+            *args: Ignored. Only for adhering to parent class interface.
+            **kwargs: Ignored. Only for adhering to parent class interface.
+        Returns:
+            The labeled test data loader.
+        """
+        return self.labeled.test_dataloader()
+
+    def _to_dataset(self, split: str) -> "AdaptionDataset":
+        labeled = self.labeled.to_dataset(split)
+        unlabeled = self.unlabeled.to_dataset(split)
+        dataset = AdaptionDataset(labeled, unlabeled)
+
+        return dataset
