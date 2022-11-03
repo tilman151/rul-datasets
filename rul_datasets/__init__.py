4c7247e17c1659a9b09b53be32492ecb58448dc4
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-11-03 22:43:43 +0100

    Set version automatically on release. (#4)

--- rul_datasets/__init__.py
+++ rul_datasets/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.0"
+__version__ = "0.0.0"
 
 from .adaption import DomainAdaptionDataModule, PretrainingAdaptionDataModule
 from .baseline import BaselineDataModule, PretrainingBaselineDataModule
