cfffd04a3cfd8409e16aa8f62afc2716729aa0a2
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-11-03 21:04:56 +0100

    Update root init.

--- rul_datasets/__init__.py
+++ rul_datasets/__init__.py
@@ -2,5 +2,6 @@ __version__ = "0.3.0"
 
 from .adaption import DomainAdaptionDataModule, PretrainingAdaptionDataModule
 from .baseline import BaselineDataModule, PretrainingBaselineDataModule
+from .ssl import SemiSupervisedDataModule
 from .core import RulDataModule
-from .reader import CmapssReader, FemtoReader
+from .reader import CmapssReader, FemtoReader, XjtuSyReader
