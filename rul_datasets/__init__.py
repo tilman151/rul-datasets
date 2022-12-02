216f0beeaaf3f4fe39d531040b582c383dcbd390
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-12-02 09:41:15 +0100

    fix: suppress dataloader num_worker warning
    data is held in memory which makes multiple data loaders unnecessary.

--- rul_datasets/__init__.py
+++ rul_datasets/__init__.py
@@ -1,8 +1,14 @@
 __version__ = "0.0.0"
 
+import warnings
+
 from .adaption import DomainAdaptionDataModule, PretrainingAdaptionDataModule
 from .baseline import BaselineDataModule, PretrainingBaselineDataModule
-from .ssl import SemiSupervisedDataModule
 from .core import RulDataModule
 from .reader import CmapssReader, FemtoReader, XjtuSyReader
 from .reader.data_root import get_data_root, set_data_root
+from .ssl import SemiSupervisedDataModule
+
+warnings.filterwarnings(
+    "ignore", ".*Consider increasing the value of the `num_workers` argument*"
+)
