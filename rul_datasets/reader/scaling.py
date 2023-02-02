26daf7296979c84a296e2b20380cb7ede7873168
Tilman Krokotsch <t.krokotsch@gmail.com>
2023-02-02 10:47:02 +0100

    feat: add operation condition aware scaling to CMAPSS

--- rul_datasets/reader/scaling.py
+++ rul_datasets/reader/scaling.py
@@ -1,11 +1,11 @@
 """A module with functions for scaling RUL features."""
-
+import copy
 import pickle
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Tuple
 
 import numpy as np
 from sklearn import preprocessing as scalers  # type: ignore
-
+from sklearn.base import BaseEstimator, TransformerMixin
 
 Scaler = Union[
     scalers.StandardScaler,
@@ -23,7 +23,61 @@ Supported scalers:
 """
 
 
-def fit_scaler(features: List[np.ndarray], scaler: Optional[Scaler] = None) -> Scaler:
+class OperationConditionAwareScaler(BaseEstimator, TransformerMixin):
+    def __init__(
+        self, base_scaler: Scaler, boundaries: List[Tuple[float, float]]
+    ) -> None:
+        self.base_scalers = [copy.deepcopy(base_scaler) for _ in boundaries]
+        self.boundaries = boundaries
+
+    @property
+    def n_features_in_(self):
+        return self.base_scalers[0].n_features_in_
+
+    def partial_fit(
+        self, features: np.ndarray, operation_conditions: np.ndarray
+    ) -> "OperationConditionAwareScaler":
+        total = 0
+        for i, (lower, upper) in enumerate(self.boundaries):
+            idx = self._between(operation_conditions, lower, upper)
+            if num_elem := np.sum(idx):  # guard against empty array
+                self.base_scalers[i].partial_fit(features[idx])
+                total += num_elem
+        self._check_all_transformed(features, total, "fitted")
+
+        return self
+
+    def transform(
+        self, features: np.ndarray, operation_conditions: np.ndarray
+    ) -> np.ndarray:
+        scaled = np.empty_like(features)
+        total = 0
+        for i, (lower, upper) in enumerate(self.boundaries):
+            idx = self._between(operation_conditions, lower, upper)
+            if num_elem := np.sum(idx):  # guard against empty array
+                scaled[idx] = self.base_scalers[i].transform(features[idx])
+                total += num_elem
+        self._check_all_transformed(features, total, "scaled")
+
+        return scaled
+
+    def _check_all_transformed(self, features, total, activity):
+        """Guard against unknown conditions"""
+        if diff := (len(features) - total):
+            raise RuntimeError(
+                f"{diff} samples had an unknown condition and could not be {activity}."
+                "Please adjust the boundaries."
+            )
+
+    def _between(self, inputs: np.ndarray, lower: float, upper: float) -> np.ndarray:
+        return (lower <= inputs) & (inputs <= upper)
+
+
+def fit_scaler(
+    features: List[np.ndarray],
+    scaler: Optional[Union[Scaler, OperationConditionAwareScaler]] = None,
+    operation_conditions: Optional[List[np.ndarray]] = None,
+) -> Union[Scaler, OperationConditionAwareScaler]:
     """
     Fit a given scaler to the RUL features. If the scaler is omitted,
     a StandardScaler will be created.
@@ -36,11 +90,38 @@ def fit_scaler(features: List[np.ndarray], scaler: Optional[Scaler] = None) -> S
     Returns:
         The fitted scaler
     """
+    if operation_conditions is None:
+        scaler = _fit_scaler_naive(features, scaler)
+    else:
+        scaler = _fit_scaler_operation_condition_aware(
+            features, scaler, operation_conditions
+        )
+
+    return scaler
+
+
+def _fit_scaler_naive(features: List[np.ndarray], scaler: Optional[Scaler]) -> Scaler:
     if scaler is None:
         scaler = scalers.StandardScaler()
     for run in features:
         run = run.reshape(-1, run.shape[-1])
         scaler.partial_fit(run)
+
+    return scaler
+
+
+def _fit_scaler_operation_condition_aware(
+    features: List[np.ndarray],
+    scaler: OperationConditionAwareScaler,
+    operation_conditions: List[np.ndarray],
+) -> OperationConditionAwareScaler:
+    assert len(features[0].shape) == 2, "Condition aware scaling can't fit window data"
+    if scaler is None:
+        raise ValueError(
+            "Scaler must be supplied if it should be operation condition aware."
+        )
+    for run, cond in zip(features, operation_conditions):
+        scaler.partial_fit(run, cond)
 
     return scaler
 
@@ -72,7 +153,11 @@ def load_scaler(save_path: str) -> Scaler:
     return scaler
 
 
-def scale_features(features: List[np.ndarray], scaler: Scaler) -> List[np.ndarray]:
+def scale_features(
+    features: List[np.ndarray],
+    scaler: Union[Scaler, OperationConditionAwareScaler],
+    operation_conditions: Optional[List[np.ndarray]] = None,
+) -> List[np.ndarray]:
     """
     Scaler the RUL features with a given scaler.
 
@@ -86,6 +171,20 @@ def scale_features(features: List[np.ndarray], scaler: Scaler) -> List[np.ndarra
     Returns:
         The scaled features.
     """
+    if operation_conditions is None:
+        features = _scale_features_naive(features, scaler)
+    else:
+        features = _scale_features_condition_aware(
+            features, scaler, operation_conditions
+        )
+
+    return features
+
+
+def _scale_features_naive(
+    features: List[np.ndarray], scaler: Scaler
+) -> List[np.ndarray]:
+    features = copy.copy(features)
     for i, run in enumerate(features):
         _check_channels(run, scaler)
         if len(run.shape) == 3:
@@ -96,7 +195,23 @@ def scale_features(features: List[np.ndarray], scaler: Scaler) -> List[np.ndarra
     return features
 
 
-def _check_channels(run: np.ndarray, scaler: Scaler) -> None:
+def _scale_features_condition_aware(
+    features: List[np.ndarray],
+    scaler: OperationConditionAwareScaler,
+    operation_conditions: List[np.ndarray],
+) -> List[np.ndarray]:
+    assert len(features[0].shape) == 2, "No condition aware scaling for window data"
+    features = copy.copy(features)
+    for i, (run, cond) in enumerate(zip(features, operation_conditions)):
+        _check_channels(run, scaler)
+        features[i] = scaler.transform(run, cond)
+
+    return features
+
+
+def _check_channels(
+    run: np.ndarray, scaler: Union[Scaler, OperationConditionAwareScaler]
+) -> None:
     if not run.shape[-1] == scaler.n_features_in_:
         raise ValueError(
             f"The scaler was fit on {scaler.n_features_in_} "
--- tests/reader/test_scaling.py
+++ tests/reader/test_scaling.py
@@ -1,5 +1,6 @@
 import os.path
 from typing import Tuple
+from unittest import mock
 
 import numpy as np
 import numpy.testing as npt
@@ -19,6 +20,71 @@ def fitted_scaler():
     return scaler
 
 
+@pytest.fixture()
+def conditioned_inputs():
+    cond_idx = range(1, 4)
+    features = [np.random.choice([-i, i], (10, 5)) for i in cond_idx]
+    conditions = [np.ones(10) * i for i in cond_idx]
+    boundaries = [(i, i + 0.9) for i in cond_idx]
+
+    return features, conditions, boundaries
+
+
+@pytest.fixture()
+def fitted_conditioned_scaler(conditioned_inputs):
+    features, conditions, boundaries = conditioned_inputs
+    features = np.concatenate(features)
+    conditions = np.concatenate(conditions)
+    scaler = scaling.OperationConditionAwareScaler(scalers.MinMaxScaler(), boundaries)
+    scaler = scaler.partial_fit(features, conditions)
+
+    return scaler
+
+
+class TestOperationConditionAwareScaler:
+    def test_partial_fit(self, conditioned_inputs):
+        features, conditions, boundaries = conditioned_inputs
+        scaler = scaling.OperationConditionAwareScaler(
+            scalers.MinMaxScaler(), boundaries
+        )
+
+        for f, c in zip(features, conditions):
+            scaler.partial_fit(f, c)
+
+        assert len(scaler.base_scalers) == len(boundaries)
+        for i in range(len(boundaries)):
+            assert np.all(scaler.base_scalers[i].data_min_ == features[i].min(0))
+            assert np.all(scaler.base_scalers[i].data_max_ == features[i].max(0))
+
+    def test_partial_fit_unknown_condition(self, conditioned_inputs):
+        features, conditions, boundaries = conditioned_inputs
+        conditions[0][5] = 99  # condition not covered by boundaries
+        scaler = scaling.OperationConditionAwareScaler(
+            scalers.MinMaxScaler(), boundaries
+        )
+
+        with pytest.raises(RuntimeError):
+            for f, c in zip(features, conditions):
+                scaler.partial_fit(f, c)
+
+    def test_transform(self, fitted_conditioned_scaler):
+        num_conditions = len(fitted_conditioned_scaler.boundaries)
+        conditions = np.random.choice(range(1, num_conditions + 1), 100)
+        features = np.ones((100, 5)) * conditions[:, None]
+
+        scaled = fitted_conditioned_scaler.transform(features, conditions)
+        npt.assert_almost_equal(scaled, 1)
+
+    def test_transform_unknown_condition(self, fitted_conditioned_scaler):
+        num_conditions = len(fitted_conditioned_scaler.boundaries)
+        conditions = np.random.choice(range(1, num_conditions + 1), 100)
+        conditions[5] = 99  # condition not covered by boundaries
+        features = np.random.randn(100, 5)
+
+        with pytest.raises(RuntimeError):
+            fitted_conditioned_scaler.partial_fit(features, conditions)
+
+
 @pytest.mark.parametrize("feature_shape", [(1000, 5), (1000, 2, 5)])
 def test_fit_scaler(feature_shape: Tuple[int]):
     features = [np.random.randn(*feature_shape) * 2 + 1 for _ in range(10)]
@@ -33,6 +99,19 @@ def test_fit_scaler_custom_scaler():
     scaler = scaling.fit_scaler(features, scalers.MinMaxScaler())
 
     assert isinstance(scaler, scalers.MinMaxScaler)
+
+
+def test_fit_condition_aware_scaler():
+    features = [np.random.uniform(0, 3, (10, 5)), np.random.uniform(0, 2, (10, 5))]
+    operation_conditions = [np.zeros(10), np.ones(10)]
+    scaler = mock.MagicMock(scaling.OperationConditionAwareScaler)
+
+    out_scaler = scaling.fit_scaler(features, scaler, operation_conditions)
+
+    assert scaler is out_scaler
+    scaler.partial_fit.assert_has_calls(
+        [mock.call(f, c) for f, c in zip(features, operation_conditions)]
+    )
 
 
 def test_save_load_scaler(tmp_path, fitted_scaler):
@@ -60,3 +139,27 @@ def test_scale_features(feature_shape, fitted_scaler):
     assert scaled_features.shape == feature_shape
     npt.assert_almost_equal(np.mean(scaled_features), 0.0, decimal=2)
     npt.assert_almost_equal(np.std(scaled_features), 1.0, decimal=2)
+
+
+@pytest.mark.parametrize(
+    "feature_shape",
+    [
+        (10000, 5),
+        pytest.param((5000, 2, 5), marks=pytest.mark.xfail),
+        pytest.param((5000, 4), marks=pytest.mark.xfail),
+    ],
+)
+def test_scale_features_condition_aware(feature_shape, fitted_conditioned_scaler):
+    fitted_conditioned_scaler.transform = mock.MagicMock(name="transform")
+    features = [np.random.randn(*feature_shape)]
+    operation_condition = [np.ones(feature_shape[0])]
+
+    scaled = scaling.scale_features(
+        features, fitted_conditioned_scaler, operation_condition
+    )
+
+    fitted_conditioned_scaler.transform.assert_called_with(
+        features[0], operation_condition[0]
+    )
+    assert isinstance(scaled, list)
+    assert scaled[0] is fitted_conditioned_scaler.transform()
